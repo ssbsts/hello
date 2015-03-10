693e1b7c44cb56203100d56b0a33a02ef16f4e25
ssbsts <acottle@gmail.com>
2015-03-10 16:48:02 -0600

    Hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+// 3/9/15
+// My first java program
+
+public class Hello {
+
+  // Prints out a greeting.
+  public static void main(String[] args) {
+
+    System.out.println("Hello Tester!");
+  }
+}
